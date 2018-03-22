97592ecc9cef811ff3eb69e700fd5a32b96e6079
Luis Espinoza <luis.espinoza@buildbinder.com>
2018-03-22 13:27:56 -0700

    modules

---
+++ src/app/app-footer/app-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-footer',
+  templateUrl: './app-footer.component.html',
+  styleUrls: ['./app-footer.component.css']
+})
+export class AppFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
+export const routes: Routes = [
+  // { path: '', redirectTo: '/services', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'services', component: ServicesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    HomeComponent,
+    AppHeaderComponent,
+    AppFooterComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    HttpClientModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash: true}),
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
